0347a2e96b5f0e08863a70a09709efab60000b2f
webdevmatias <lucasmatiassilva2022@gmail.com>
2026-03-15 17:13:22 -0300

    feat: hashPassword com argon2; password + pepper

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private configService: ConfigService) {}
 
+  // Metodo para obter o pepper
   private getPepper(): string {
     return this.configService.getOrThrow<string>('PASSWORD_PEPPER');
   }
+
+  // Hash da senha com pepper
+  private async hashPassword(password: string): Promise<string> {
+    const pepper = this.getPepper();
+    // Concatena a senha com o pepper antes de hashing
+    const pepperedPassword = password + pepper;
+    // Opções recomendadas para Argon2id
+    return argon2.hash(pepperedPassword, {
+      type: argon2.argon2id,
+      memoryCost: 2 ** 16, // 64 MB
+      timeCost: 3,
+      parallelism: 1,
+    });
+  }
+
+  // Verificar senha
+  // Registro
+  // Gerar token JWT
+  // login
+  // Método usado pelo JwtStrategy para validar o usuário
 
   getHello(): string {
     return 'auth service';
